4e93cadfece4c71063222c5de20b771169720938
habberrih <abdul2013ly@gmail.com>
2023-08-19 13:37:30 +0200

    test: add test function

--- chat-api/src/app.service.ts
+++ chat-api/src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): Object {
+    return { title: "Hello from the backend!" };
   }
 }

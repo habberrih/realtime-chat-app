1ef6bf04312cbfef8591a53f7808a4af54ab1f0c
habberrih <abdul2013ly@gmail.com>
2023-08-20 20:39:28 +0200

    refactor: change id

--- chat-api/src/user/model/user.entity.ts
+++ chat-api/src/user/model/user.entity.ts
@@ -2,7 +2,7 @@ import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class UserEntity {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
   @Column({ nullable: false, unique: true })

07cd8c536e5ebdb0839e1249ca5db13974eb4d5d
habberrih <abdul2013ly@gmail.com>
2023-08-20 20:34:16 +0200

    refactor: remove username

--- chat-api/src/app.module.ts
+++ chat-api/src/app.module.ts
@@ -1,19 +1,17 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: "postgres",
+      type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      // username: process.env.POSTGRES_USER,
-      // password: process.env.POSTGRES_PASSWORD,
       synchronize: true,
     }),
     UserModule,

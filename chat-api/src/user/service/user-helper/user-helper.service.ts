da5fae02e32da515c82bd49f1499084097f1d29d
habberrih <abdul2013ly@gmail.com>
2023-08-20 20:39:14 +0200

    refactor: remove pipes

--- chat-api/src/user/service/user-helper/user-helper.service.ts
+++ chat-api/src/user/service/user-helper/user-helper.service.ts
@@ -1,25 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { Observable, of } from 'rxjs';
+
 import { CreateUserDto } from 'src/user/model/dto/createUser.dto';
 import { LoginUserDto } from 'src/user/model/dto/login-user.dto';
 import { UserInterface } from 'src/user/model/user.interface';
 
 @Injectable()
 export class UserHelperService {
-  createUserDtoToEntity(
-    createUserDto: CreateUserDto
-  ): Observable<UserInterface> {
-    return of({
+  createUserDtoToEntity(createUserDto: CreateUserDto): UserInterface {
+    return {
       email: createUserDto.email,
       username: createUserDto.username,
       password: createUserDto.password,
-    });
+    };
   }
 
-  loginUserDtoToEntity(loginUserDto: LoginUserDto): Observable<UserInterface> {
-    return of({
+  loginUserDtoToEntity(loginUserDto: LoginUserDto): UserInterface {
+    return {
       email: loginUserDto.email,
       password: loginUserDto.password,
-    });
+    };
   }
 }

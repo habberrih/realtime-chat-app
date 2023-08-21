b877cdf11a9ce7a335e47aae76f3ccd63d9857d2
habberrih <abdul2013ly@gmail.com>
2023-08-21 14:23:42 +0200

    feat: add auth module

---
+++ chat-api/src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './service/auth.service';
+import { AuthController } from './controller/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtAuthGuard } from './guards/jwt.guard';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
+  exports: [AuthService],
+  //controllers: [AuthController],
+})
+export class AuthModule {}

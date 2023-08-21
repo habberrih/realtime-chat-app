2979e31a2bfc2b4af999efc6e9a6199ae3a2cb7e
habberrih <abdul2013ly@gmail.com>
2023-08-21 14:24:31 +0200

    feat: add AuthService class

---
+++ chat-api/src/auth/service/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { UserInterface } from 'src/user/model/user.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async generateJwtToken(user: UserInterface): Promise<string> {
+    return this.jwtService.signAsync({ user });
+  }
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
+
+  async comparePasswords(
+    password: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+}

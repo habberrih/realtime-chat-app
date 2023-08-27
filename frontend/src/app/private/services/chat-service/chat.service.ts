7519c1bcce430ec532d1c904500d3cb84ced61ee
habberrih <abdul2013ly@gmail.com>
2023-08-27 15:17:47 +0200

    refactor: connect to backend via socket

--- frontend/src/app/private/services/chat-service/chat.service.ts
+++ frontend/src/app/private/services/chat-service/chat.service.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
+import { CustomSocket } from '../../sockets/custom-socket';
+import {
+  RoomInterface,
+  RoomPaginateInterface,
+} from 'src/app/model/room.interface';
+import { UserInterface } from 'src/app/model/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
-  constructor(private socket: Socket) {}
+  constructor(private socket: CustomSocket) {}
 
-  async sendMessage() {}
+  sendMessage() {}
 
-  async getMessages() {
+  getMessages() {
     return this.socket.fromEvent('message');
+  }
+
+  getMyRooms() {
+    return this.socket.fromEvent<RoomPaginateInterface>('rooms');
+  }
+
+  createRoom() {
+    const user2: UserInterface = {
+      id: 'a2aabe55-d56f-4209-83e2-351f2d3ac36d',
+    };
+    const room: RoomInterface = {
+      name: 'room1',
+      users: [user2],
+    };
+
+    this.socket.emit('createRoom', room);
   }
 }

96bfb7bf94588d5824a3dbdee4d3da5362ee266f
habberrih <abdul2013ly@gmail.com>
2023-08-28 13:29:07 +0200

    refactor: display rooms in frontend

--- frontend/src/app/private/components/dashboard/dashboard.component.ts
+++ frontend/src/app/private/components/dashboard/dashboard.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ChatService } from '../../services/chat-service/chat.service';
-import { RoomPaginateInterface } from 'src/app/model/room.interface';
+
+import { MatSelectionListChange } from '@angular/material/list';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
   rooms$ = this.chatService.getMyRooms();
+  selectedRoom = null;
 
   constructor(private chatService: ChatService) {}
 
-  ngOnInit() {
-    this.chatService.createRoom();
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    this.chatService.emitPaginateRooms(10, 0);
+  }
+
+  onSelectRoom(event: MatSelectionListChange) {
+    this.selectedRoom = event.source.selectedOptions.selected[0].value;
+  }
+
+  onPaginateRooms(pageEvent: PageEvent) {
+    this.chatService.emitPaginateRooms(pageEvent.pageSize, pageEvent.pageIndex);
   }
 }

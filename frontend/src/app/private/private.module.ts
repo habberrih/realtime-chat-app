95d2e421a80bdc8c8c16c5062527d354f35cae23
habberrih <abdul2013ly@gmail.com>
2023-08-28 13:27:36 +0200

    refactor: add some modules

--- frontend/src/app/private/private.module.ts
+++ frontend/src/app/private/private.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { PrivateRoutingModule } from './private-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [CommonModule, PrivateRoutingModule, MatListModule],
+  declarations: [DashboardComponent, CreateRoomComponent],
+  imports: [
+    CommonModule,
+    PrivateRoutingModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
 export class PrivateModule {}

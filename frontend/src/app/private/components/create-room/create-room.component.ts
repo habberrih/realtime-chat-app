d1746649ee7f8d5ec3ec7a6d34239f6669ad29c8
habberrih <abdul2013ly@gmail.com>
2023-08-29 16:28:11 +0200

    refactor: create room and add users to it

--- frontend/src/app/private/components/create-room/create-room.component.ts
+++ frontend/src/app/private/components/create-room/create-room.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserInterface } from 'src/app/model/user.interface';
+import { ChatService } from '../../services/chat-service/chat.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-room',
   templateUrl: './create-room.component.html',
-  styleUrls: ['./create-room.component.scss']
+  styleUrls: ['./create-room.component.scss'],
 })
 export class CreateRoomComponent {
+  form: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    description: new FormControl(null),
+    users: new FormArray([], [Validators.required]),
+  });
 
+  constructor(
+    private chatService: ChatService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  get name(): FormControl {
+    return this.form.get('name') as FormControl;
+  }
+
+  get description(): FormControl {
+    return this.form.get('description') as FormControl;
+  }
+
+  get users(): FormArray {
+    return this.form.get('users') as FormArray;
+  }
+
+  createChat() {
+    if (this.form.valid) {
+      this.chatService.createRoom(this.form.getRawValue());
+      this.router.navigate(['../dashboard'], {
+        relativeTo: this.activatedRoute,
+      });
+    }
+  }
+
+  initUser(user: UserInterface) {
+    return new FormControl({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    });
+  }
+  addUser(userFormControl: FormControl) {
+    this.users.push(userFormControl);
+  }
+
+  removeUser(userId: string) {
+    this.users.removeAt(
+      this.users.value.findIndex((user: UserInterface) => user.id === userId)
+    );
+  }
 }

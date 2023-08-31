36093e7600de27db442c550c85fb691a8073bf4a
habberrih <abdul2013ly@gmail.com>
2023-08-31 16:19:42 +0200

    chore: script to clear all data from db

---
+++ chat-api/src/prisma/clear-database.ts
@@ -0,0 +1,16 @@
+import { INestApplication } from '@nestjs/common';
+import { EntityManager } from 'typeorm';
+
+async function clearDatabase(app: INestApplication): Promise<void> {
+  const entityManager = app.get<EntityManager>(EntityManager);
+  const tableNames = entityManager.connection.entityMetadatas
+    .map((entity) => entity.tableName)
+    .join(', ');
+
+  await entityManager.query(`truncate ${tableNames} restart identity cascade;`);
+}
+
+let app: INestApplication;
+
+const clean = async () => await clearDatabase(app);
+clean();

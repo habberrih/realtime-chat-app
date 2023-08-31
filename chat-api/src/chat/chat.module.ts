ac502dab6b381789f0ccab6ad38bf5e16048456e
habberrih <abdul2013ly@gmail.com>
2023-08-31 16:18:17 +0200

    refactor: add connected user entity and service

--- chat-api/src/chat/chat.module.ts
+++ chat-api/src/chat/chat.module.ts
@@ -5,9 +5,15 @@ import { UserModule } from 'src/user/user.module';
 import { RoomService } from './service/room-service/room.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoomEntity } from './model/room.entity';
+import { ConnectedUserService } from './service/connected-user/connected-user.service';
+import { ConnectedUserEntity } from './model/connected-user.entity';
 
 @Module({
-  imports: [AuthModule, UserModule, TypeOrmModule.forFeature([RoomEntity])],
-  providers: [ChatGateway, RoomService],
+  imports: [
+    AuthModule,
+    UserModule,
+    TypeOrmModule.forFeature([RoomEntity, ConnectedUserEntity]),
+  ],
+  providers: [ChatGateway, RoomService, ConnectedUserService],
 })
 export class ChatModule {}
